a5e7df9f91327a22deed70ac5279edc00cf9823e
elenalynx <elnlynx@gmail.com>
2022-10-26 16:46:59 +0300

    added tasks solutions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+  // __________1-й ВАРІАНТ____________
+// export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+//
+//   return (minFirst <= minSecond ? minFirst : minSecond)
+//     * (maxFirst >= maxSecond ? maxFirst : maxSecond);
+// };
+
+  // __________2-й ВАРІАНТ____________
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+
+    return Math.min(minFirst, minSecond)
+      * Math.max(maxFirst, maxSecond);
+  };
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +28,10 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+
+  return Math.PI * Math.pow(radius, 2);
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +41,15 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+  let roundingFactor = '1';
+  for (let i = 0; i < accuracy; i += 1) {
+    roundingFactor += '0';
+  }
+  roundingFactor = +roundingFactor;
+
+  return Math.round(number * roundingFactor) / roundingFactor;
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +57,10 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+
+  return Math.round(number) % 2 === 0 ? Math.floor(number) : Math.ceil(number);
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +70,10 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
+
+// НЮАНС в описі останньої задачі - "...от какого и ДОООО какого числа.." Не повинно бути ДО, повинно бути ПО таке то число, тоді тести будуть коректні)))
